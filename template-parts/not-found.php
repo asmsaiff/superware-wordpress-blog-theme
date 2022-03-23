1b428a802b15c07e0bfabec54eb9d80d81e5e6ee
saifullahsiddique <info@saifullah.co>
2022-03-23 08:24:38 +0600

    404 page design

---
+++ template-parts/not-found.php
@@ -0,0 +1,17 @@
+<div class="error-not-found">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="text-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/error-404.png" class="not-found-img" alt="">
+
+                    <h1 class="sitetitle my-4">
+                        <?php
+                            _e("Sorry! There has no content in this query!!!", "mediumish");
+                        ?>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
