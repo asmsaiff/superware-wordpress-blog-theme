c59ebbcab5aac3da1a613126c95e2963043c98cd
saifullahsiddique <info@saifullah.co>
2022-03-23 07:23:22 +0600

    add skip links scripts

--- functions.php
+++ functions.php
@@ -59,3 +59,13 @@
         wp_enqueue_script( "mediumish-js", get_template_directory_uri() . "/assets/js/mediumish.js", array("jquery"), VERSION, true );
     }
     add_action( "wp_enqueue_scripts", "mediumish_assets" );
+
+    function mediumish_skip_link_focus_fix() {
+        // The following is minified via `terser --compress --mangle -- js/skip-link-focus-fix.js`.
+    ?>
+        <script>
+            /(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+        </script>
+    <?php
+    }
+    add_action( 'wp_print_footer_scripts', 'mediumish_skip_link_focus_fix' );
