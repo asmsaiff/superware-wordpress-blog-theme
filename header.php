1cccdf72205b73340aa6eda132a6457371d53352
saifullahsiddique <info@saifullah.co>
2022-03-27 23:30:30 +0600

    translate area-label and escapped it

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
         </a>
         <div class="container navContainer ml-0 ml-sm-auto">
             <button class="navbar-toggler navbar-toggler-right shadow-none border-0" type="button" data-toggle="collapse"
-                data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="<?php _e("Toggle navigation", "superware"); ?>">
+                data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="<?php esc_attr_e("Toggle navigation", "superware"); ?>">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
