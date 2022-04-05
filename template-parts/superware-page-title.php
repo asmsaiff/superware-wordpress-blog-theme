dc915d994d944a6aabbee718b7cee3cf9f10e564
saidmsaif <saidmsaiff@gmail.com>
2022-04-05 15:19:13 +0600

    page title area dynamic

--- template-parts/superware-page-title.php
+++ template-parts/superware-page-title.php
@@ -1,8 +1,12 @@
 <div class="mainheading">
     <h1 class="sitetitle">
-        <?php bloginfo( 'name' ); ?>
+        <?php
+            esc_html_e(get_theme_mod('superware__page_title_settings', 'SuperWare'));
+        ?>
     </h1>
     <p class="lead">
-        <?php bloginfo( 'description' ) ?>
+        <?php
+            esc_html_e(get_theme_mod('superware__page_desc_settings', 'SuperWare is a simple wordpress blog theme with clean layout.'));
+        ?>
     </p>
 </div>
